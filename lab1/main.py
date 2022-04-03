83788802e27440a56cf3dd0b34dbf6696f5861b4
rbosko <prajmik@gmail.com>
2022-04-03 11:43:30 +0200

    lab1 v02

--- lab1/main.py
+++ lab1/main.py
@@ -1,20 +1,6 @@
 import sys
 from Bio import SeqIO
+from Bio.SeqUtils import GC
 
-filename=sys.argv[1]
-
-fh= open(filename,'r')
-
-parser = SeqIO.parse(fh, "fasta")
-
-gc = 0
-total = 0
-for record in parser:
-    for x in str(record.seq):
-        if x == 'G' or x == 'C':
-            gc +=1    
-    total += len(record.seq)
-
-gc_content = gc * 100 / total
-
-print("GC: {}%".format(gc_content))
+fh= next(SeqIO.parse(sys.argv[1], "fasta"))
+print(GC(fh.seq))
